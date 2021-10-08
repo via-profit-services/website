280556c43f195c17737ef8cdc4e6998d50adc8f2
Vasily Novosad <delhsmail@gmail.com>
2021-10-08 11:36:12 +0500

    WIP

--- src/render/desktop/containers/Docs/core/v2.0.x/middlewares.tsx
+++ src/render/desktop/containers/Docs/core/v2.0.x/middlewares.tsx
@@ -1,17 +1,68 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
+import { useIntl, FormattedMessage } from 'react-intl';
 
-import H1 from '~/render/desktop/components/Typography/H1';
+import RenderMarkdown from '~/render/desktop/components/RenderMarkdown';
+import Breadcrumbs, { Crumb } from '~/render/desktop/components/Breadcrumbs';
+import content from 'docs/core/v2.0.x/middlewares.md';
 
-const Middlewares: React.FC = () => (
-  <>
-    <Helmet>
-      <title>Core v2.0.x middlewares</title>
-      <meta name="description" content="Core middlewares" />
-    </Helmet>
+const Introduction: React.FC = () => {
+  const intl = useIntl();
 
-    <H1>Middlewares in Core v2.0.x</H1>
-  </>
-);
+  return (
+    <section>
+      <Helmet>
+        <title>
+          {intl.formatMessage({
+            defaultMessage: 'Middlewares',
+            description: 'Meta title of core.v2.0 middlewares',
+          })}
+        </title>
+        <meta
+          name="description"
+          content={intl.formatMessage({
+            defaultMessage: 'Core v2 middlewares',
+            description: 'Meta description of core.v2.0 middlewares',
+          })}
+        />
+      </Helmet>
+      <nav>
+        <Breadcrumbs>
+          <Crumb home />
+          <Crumb
+            link="/docs"
+            position={2}
+            label={
+              <FormattedMessage
+                defaultMessage="Docs"
+                description="Breadcrumbs. Docs"
+              />
+            }
+          />
+          <Crumb
+            link="/docs/core/v2.0.x"
+            position={3}
+            label={
+              <FormattedMessage
+                defaultMessage="Core v2.0"
+                description="Breadcrumbs. Core v2.0.x"
+              />
+            }
+          />
+          <Crumb
+            position={4}
+            label={
+              <FormattedMessage
+                defaultMessage="Middlewares"
+                description="Breadcrumbs. Core v2.0.x middlewares"
+              />
+            }
+          />
+        </Breadcrumbs>
+      </nav>
+      <RenderMarkdown>{content}</RenderMarkdown>
+    </section>
+  );
+};
 
-export default Middlewares;
+export default Introduction;
