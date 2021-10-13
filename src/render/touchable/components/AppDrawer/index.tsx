8ef15fd5288e9ab761e8a059511eefd48aa4ba56
Vasily Novosad <delhsmail@gmail.com>
2021-10-13 12:02:58 +0500

    WIP

--- src/render/touchable/components/AppDrawer/index.tsx
+++ src/render/touchable/components/AppDrawer/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import SwipableViews from 'react-swipeable-views';
-import HomeIcon from 'mdi-react/HomeOutlineIcon';
-import AboutIcon from 'mdi-react/BlurIcon';
+import CloseIcon from 'mdi-react/CloseIcon';
+import { useIntl, FormattedMessage } from 'react-intl';
 
 import DrawerItem from './DrawerItem';
 
@@ -43,14 +43,42 @@ const DrawerFallback = styled.div`
   width: 100%;
 `;
 
-const DrawerHeader = styled.div`
-  background-color: ${props => props.theme.color.accent.primary};
-  background-position: center center;
-  background-size: cover;
-  height: 160px;
+const Header = styled.header`
+  box-shadow: ${props => props.theme.shadows[0]};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  font-size: 1.1rem;
+  font-weight: 800;
+  padding: 1em;
+`;
+
+const SeparatorTitle = styled.div`
+  font-size: 0.9rem;
+  padding: 1em;
+  margin-top: 1em;
+  color: ${({ theme }) => theme.color.text.secondary};
+`;
+
+const CloseButton = styled.button`
+  outline: none;
+  border: 0;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 3.5em;
+  font-size: 1rem;
+  color: ${({ theme }) => theme.color.text.primary};
 `;
 
 const AppDrawer: React.FC<AppDrawerProps> = props => {
+  const intl = useIntl();
   const { open, onClose } = props;
   const [visibility, setVisibility] = React.useState(open);
   const [activeIndex, setActiveIndex] = React.useState(open ? 0 : 1);
@@ -72,20 +100,110 @@ const AppDrawer: React.FC<AppDrawerProps> = props => {
           }
         }}>
         <DrawerInner>
-          <DrawerHeader />
+          <Header>
+            <Title>
+              <FormattedMessage
+                defaultMessage="Main menu"
+                description="Main sidebar title"
+              />
+            </Title>
+            <CloseButton
+              type="button"
+              onClick={() => onClose()}
+              aria-label={intl.formatMessage({
+                defaultMessage: 'Close docs menu',
+                description:
+                  'Main menu. «aria-label» attribute of close button',
+              })}>
+              <CloseIcon size="1em" color="currentColor" />
+            </CloseButton>
+          </Header>
 
           <DrawerNav>
             <DrawerItem
               to="/"
-              icon={<HomeIcon size="1em" color="currentColor" />}
-              label="Home"
               onDrawerClose={onClose}
+              label={intl.formatMessage({
+                defaultMessage: 'Home',
+                description: 'Main menu. Home link',
+              })}
             />
             <DrawerItem
               to="/docs"
-              icon={<AboutIcon size="1em" color="currentColor" />}
-              label="Docs"
               onDrawerClose={onClose}
+              label={intl.formatMessage({
+                defaultMessage: 'Documentation',
+                description: 'Main menu. Docs link',
+              })}
+            />
+            <DrawerItem
+              to="/examples"
+              onDrawerClose={onClose}
+              label={intl.formatMessage({
+                defaultMessage: 'Examples',
+                description: 'Main menu. Examples link',
+              })}
+            />
+            <SeparatorTitle>
+              <FormattedMessage
+                defaultMessage="Packages"
+                description="Main sidebar. Packages title"
+              />
+            </SeparatorTitle>
+            <DrawerItem
+              to="/packages/core"
+              onDrawerClose={onClose}
+              label={intl.formatMessage({
+                defaultMessage: 'Core',
+                description: 'Main menu. Package core link',
+              })}
+            />
+            <DrawerItem
+              to="/packages/knex"
+              onDrawerClose={onClose}
+              label={intl.formatMessage({
+                defaultMessage: 'Knex',
+                description: 'Main menu. Package knex link',
+              })}
+            />
+            <DrawerItem
+              to="/packages/subscriptions"
+              onDrawerClose={onClose}
+              label={intl.formatMessage({
+                defaultMessage: 'Subscriptions',
+                description: 'Main menu. Package subscriptions link',
+              })}
+            />
+
+            <SeparatorTitle>
+              <FormattedMessage
+                defaultMessage="Legal"
+                description="Main sidebar. Legal title"
+              />
+            </SeparatorTitle>
+            <DrawerItem
+              to="/legal/privacy"
+              onDrawerClose={onClose}
+              label={intl.formatMessage({
+                defaultMessage: 'Privacy Policy',
+                description: 'Main menu. Package Privacy Policy link',
+              })}
+            />
+            <DrawerItem
+              to="/legal/terms"
+              onDrawerClose={onClose}
+              label={intl.formatMessage({
+                defaultMessage: 'Terms',
+                description: 'Main menu. Package Terms link',
+              })}
+            />
+            <DrawerItem
+              to="/legal/cookie"
+              onDrawerClose={onClose}
+              label={intl.formatMessage({
+                defaultMessage: 'Cookie policy',
+                description: 'Main menu. Package Cookie policy link',
+              })}
             />
           </DrawerNav>
         </DrawerInner>
